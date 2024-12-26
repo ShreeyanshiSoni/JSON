ca2bae9ea805c744f1d537b2f35f11e4bc34ffb2
Asus <Asus@LAPTOP-70RHUHDL>
2024-12-26 18:52:14 +0530

    commit

--- src/JSON/change_IMEI.java
+++ src/JSON/change_IMEI.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 
 public class change_IMEI {
 	//changes in IMEI
-
+	//changes in IMEI
     public static void main(String[] args) {
         int SRV_TRX_SNO = 114215; // Define the SRV_TRX_SNO value
 
